952b07101bc2d25b05a973ecf2c417d07adfe158
J7222 <warplanet72@gmail.com>
2024-05-06 02:04:38 +0530

    changed to port 3002

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(80, () => console.log('Server is running on port 80'))
+app.listen(3002, () => console.log('Server is running on port 3002'))
